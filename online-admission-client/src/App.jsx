705a6924521dc6dca587a2e7560c8388f333a251
shaonbishwas <shaonbishwas01@gmail.com>
2023-12-26 05:50:33 -0800

    animation added"

--- online-admission-client/src/App.jsx
+++ online-admission-client/src/App.jsx
@@ -1,19 +1,28 @@
-import { Link, Outlet } from 'react-router-dom'
-import './App.css'
-import logo from './assets/download.png'
+import { Link, Outlet } from "react-router-dom";
+import "./App.css";
+import logo from "./assets/download.png";
 function App() {
-
   return (
     <>
-    <div className='max-w-[1200px] mx-auto pt-2'><a href="https://uob.edu.bd/"><img src={logo} alt="" /></a></div>
-      <Link to='/'><h1 className='text-xl  md:text-4xl shadow-2xl  w-2/4 p-20 border-black font-bold text-center my-5 mx-auto'>Admission Fair Data Collection</h1></Link>
+      <div className="max-w-[1200px] mx-auto pt-2">
+        <a href="https://uob.edu.bd/">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+      <div className="">
+        <Link to="/">
+          <h1 className="text-xl md:text-4xl shadow-2xl animate w-2/4 md:p-20 p-10 border-black font-bold text-center my-5 mx-auto">
+            Admission Fair Data Collection
+          </h1>
+        </Link>
+      </div>
       <Outlet></Outlet>
-      <footer className='text-center mt-20'>
-        <p className='font-bold '>Powered by</p>
-        <p className='font-semibold text-gray-400'>UoB Computer Club</p>
+      <footer className="text-center mt-20">
+        <p className="font-bold ">Powered by</p>
+        <p className="font-semibold text-gray-400">UoB Computer Club</p>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
